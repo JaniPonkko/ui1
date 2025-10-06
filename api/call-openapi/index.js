e6afd8d48994a09db7efaef3fd6e2dc9d4dd4804
Jani Pönkkö <jani.ponkko@alten.fi>
2025-10-06 17:45:37 +0300

    asdc

--- api/call-openapi/index.js
+++ api/call-openapi/index.js
@@ -91,11 +91,11 @@ module.exports = async function (context, req) {
       const k = 5; // top 1 result
       const searchUrl = `${azureSearchEndpoint}/indexes/${azureSearchIndex}/docs/search?api-version=2025-08-01-preview`;
       const searchBody = {
-        vector: [{
+        vector: {
           value: embedding,
           fields: azureSearchEmbeddingField,
           k: k
-        }],
+        },
         search: userQuery, // or your own search string
         count: true,
         queryType: "simple",  // tai semantic jos haluaa vain tekstihaun ilman vektorihakua
